430df467d32a4ccca9dc2645581aefbb3ef53aa6
salehAbdessalem <salih.abdessalem@gmail.com>
2025-11-27 18:04:28 +0100

    Implement CI/CD pipelines and Github actions

--- src/app.js
+++ src/app.js
@@ -30,20 +30,22 @@ app.get('/', (req, res) => {
 });
 
 app.get('/health', (req, res) => {
-  res
-    .status(200)
-    .json({
-      status: 'OK',
-      timestamp: new Date().toISOString(),
-      uptime: process.uptime(),
-    });
+  res.status(200).json({
+    status: 'OK',
+    timestamp: new Date().toISOString(),
+    uptime: process.uptime(),
+  });
 });
 
 app.get('/api', (req, res) => {
-  res.status(200).json({ message: 'Acquisition API is running!'});
+  res.status(200).json({ message: 'Acquisitions API is running!' });
 });
 
 app.use('/api/auth', authRoutes);
 app.use('/api/users', usersRoutes);
+
+app.use((req, res) => {
+  res.status(404).json({ error: 'Route not found' });
+});
 
 export default app;
